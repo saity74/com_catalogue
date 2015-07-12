769aa3506134c822fba5e6a8a4d5c16b126e13d0
Shangin Nikolai <shanginn@gmail.com>
2015-07-13 03:29:09 +0500

    Add rows to categories

--- site/views/category/tmpl/default_subcategories.php
+++ site/views/category/tmpl/default_subcategories.php
@@ -9,32 +9,41 @@
 defined('_JEXEC') or die;
 $app = JFactory::getApplication();
 $params = $app->getParams();
+
+$num_columns = $params->get('num_columns', 3);
+$categories = array_chunk($this->category->getChildren(), $num_columns);
 ?>
 
 <div class="catalogue-categories">
-	<div class="row">
-		<?php foreach ($this->category->getChildren() as $category) : ?>
-			<?php $category->params = new JRegistry($category->params); ?>
-			<?php $clink = CatalogueHelperRoute::getCategoryRoute($category->id, $category->language); ?>
-			<?php $img = json_decode($category->params)->image; ?>
-			<div class="col-md-6 col-lg-6 col-sm-12 col-xs-12">
-				<div class="subcategory-wrapper">
-					<div class="category-image-wrapper pull-right">
-						<a href="<?php echo $clink; ?>">
-							<img src="<?php echo $img; ?>"/>
-						</a>
-					</div>
-					<h3 class="category-link-wrapper">
-						<a href="<?php echo $clink; ?>">
-							<?php echo $category->title; ?>
-						</a>
-					</h3>
-					<div class="category-desc-wrapper">
-						<?php echo $category->description; ?>
+	<?php foreach ($categories as $i => $row) : ?>
+		<div class="row clearfix row-<?php echo $i; ?>">
+			<?php foreach ($row as $category) : ?>
+				<?php $category->params = new JRegistry($category->params); ?>
+				<?php $clink = CatalogueHelperRoute::getCategoryRoute($category->id, $category->language); ?>
+				<?php $img = json_decode($category->params)->image; ?>
+				<?php
+					$bootstrapSize = round(12 / $num_columns);
+					$itemClass = "col-lg-$bootstrapSize col-md-$bootstrapSize col-sm-12 col-xs-12";
+				?>
+				<div class="<?php echo $itemClass; ?>">
+					<div class="subcategory-wrapper">
+						<div class="category-image-wrapper">
+							<a href="<?php echo $clink; ?>">
+								<img src="<?php echo $img; ?>"/>
+							</a>
+						</div>
+						<h3 class="category-link-wrapper">
+							<a href="<?php echo $clink; ?>">
+								<?php echo $category->title; ?>
+							</a>
+						</h3>
+						<div class="category-desc-wrapper">
+							<?php echo $category->description; ?>
+						</div>
 					</div>
 				</div>
-			</div>
-		<?php endforeach; ?>
-	</div>
+			<?php endforeach; ?>
+		</div>
+	<?php endforeach; ?>
 	<div class="clearfix"></div>
 </div>
