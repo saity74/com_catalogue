8084a40c35ec71e9e075875a75da03678708b4ad
buyanov <info@saity74.ru>
2015-07-16 01:57:32 +0500

    fix codestyle

--- site/layouts/catalogue/item/similar.php
+++ site/layouts/catalogue/item/similar.php
@@ -14,39 +14,41 @@ $params = $item->params;
 
 ?>
 <div class="row">
-<?php if ($item->similar_items) foreach ($item->similar_items as $similar) : ?>
-	<?php
-	$bootstrapSize = 3;
-	$itemClass = "col-lg-$bootstrapSize col-md-$bootstrapSize col-sm-12 col-xs-12";
-	$ilink = JRoute::_(CatalogueHelperRoute::getItemRoute($similar->id, $similar->catid));
-	?>
-	<div class="<?php echo $itemClass ?>">
-		<div class="catalogue-one-item white-box" itemscope="" itemtype="http://schema.org/Product">
-			<div class="catalogue-one-item-img <?php if ($similar->item_sale)
-			{
-				echo 'discount-label';
-			} ?>">
+<?php if ($item->similar_items) : ?>
+	<?php foreach ($item->similar_items as $similar) : ?>
+		<?php
+		$bootstrapSize = 3;
+		$itemClass = "col-lg-$bootstrapSize col-md-$bootstrapSize col-sm-12 col-xs-12";
+		$ilink = JRoute::_(CatalogueHelperRoute::getItemRoute($similar->id, $similar->catid));
+		?>
+		<div class="<?php echo $itemClass ?>">
+			<div class="catalogue-one-item white-box" itemscope="" itemtype="http://schema.org/Product">
+				<div class="catalogue-one-item-img <?php if ($similar->item_sale)
+				{
+					echo 'discount-label';
+				} ?>">
 
-				<?php echo JLayoutHelper::render('catalogue.category.image', $similar); ?>
+					<?php echo JLayoutHelper::render('catalogue.category.image', $similar); ?>
 
-			</div>
-			<div class="catalogue-one-item-desc">
-				<h5 itemprop="name">
-					<a class="product-name item-head"
-					   href="<?php echo $ilink; ?>"
-					   title="<?php echo $similar->title; ?>"
-					   itemprop="url"><?php echo $similar->title; ?></a>
-				</h5>
-
-				<div class="item-shortdesc">
-					<?php if (!empty($similar->introtext))
-					{
-						echo $similar->introtext;
-					} ?>
 				</div>
-				<?php echo JLayoutHelper::render('catalogue.item.price', $similar); ?>
+				<div class="catalogue-one-item-desc">
+					<h5 itemprop="name">
+						<a class="product-name item-head"
+						   href="<?php echo $ilink; ?>"
+						   title="<?php echo $similar->title; ?>"
+						   itemprop="url"><?php echo $similar->title; ?></a>
+					</h5>
+
+					<div class="item-shortdesc">
+						<?php if (!empty($similar->introtext))
+						{
+							echo $similar->introtext;
+						} ?>
+					</div>
+					<?php echo JLayoutHelper::render('catalogue.item.price', $similar); ?>
+				</div>
 			</div>
 		</div>
-	</div>
-<?php endforeach; ?>
+	<?php endforeach; ?>
+<?php endif; ?>
 </div>
